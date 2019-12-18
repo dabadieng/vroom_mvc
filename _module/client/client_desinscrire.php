ef7354807024fb9d539188f82f76fa89372b754c
daba <dieng.daba@outlook.com>
2019-12-18 15:23:29 +0100

    v2 attente vérif

--- _module/client/client_desinscrire.php
+++
@@ -1,7 +0,0 @@
-<?php
-require "../_include/inc_config.php";
-require "../_entite/client.php";
-
-extract($_GET);
-desincrire($le_id,$cl_id);
-header("location:" . hlien("client","edit","&id=$le_id") );
